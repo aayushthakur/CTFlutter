b9d15bcd5927e3591264d63bb10ad8a501383840
Aayush <aayush.thakur14@gmail.com>
2023-05-30 16:39:14 +0530

    updated dependencies & flutter

--- android/app/src/main/java/com/example/ct_flutter_integration/MainActivity.java
+++ android/app/src/main/java/com/example/ct_flutter_integration/MainActivity.java
@@ -3,4 +3,5 @@ package com.example.ct_flutter_integration;
 import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
+
 }
---
+++ android/app/src/main/java/com/example/ct_flutter_integration/MyFirebaseMessaging.java
@@ -0,0 +1,60 @@
+package com.example.ct_flutter_integration;
+
+import android.content.Intent;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.clevertap.android.sdk.pushnotification.fcm.CTFcmMessageHandler;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessaging extends FirebaseMessagingService {
+    private static final String TAG = FirebaseMessagingService.class.getName();
+
+    public MyFirebaseMessaging() {
+        super();
+    }
+
+    @NonNull
+    @Override
+    protected Intent getStartCommandIntent(@NonNull Intent intent) {
+        return super.getStartCommandIntent(intent);
+    }
+
+    @Override
+    public void handleIntent(@NonNull Intent intent) {
+        super.handleIntent(intent);
+    }
+
+    @Override
+    public void onDeletedMessages() {
+        super.onDeletedMessages();
+    }
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        Log.i(TAG, "onMessageReceived: " + remoteMessage.getData());
+        boolean status = new CTFcmMessageHandler().createNotification(getApplicationContext(), remoteMessage);
+        Log.i(TAG, "onMessageReceived status: " + status);
+
+    }
+
+    @Override
+    public void onMessageSent(@NonNull String s) {
+        super.onMessageSent(s);
+    }
+
+    @Override
+    public void onNewToken(@NonNull String s) {
+        super.onNewToken(s);
+    }
+
+    @Override
+    public void onSendError(@NonNull String s, @NonNull Exception e) {
+        super.onSendError(s, e);
+    }
+}
+
+
