4b7be582c3b82b6679ffc986984abb4786d81953
Aayush <aayush.thakur14@gmail.com>
2023-05-31 15:47:17 +0530

    added fix for pushclick not received on android 12 & above

--- android/app/src/main/java/com/example/ct_flutter_integration/MainActivity.java
+++ android/app/src/main/java/com/example/ct_flutter_integration/MainActivity.java
@@ -1,7 +1,32 @@
 package com.example.ct_flutter_integration;
 
+import android.content.Intent;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.clevertap.android.sdk.CleverTapAPI;
+import com.google.gson.Gson;
+
+import java.util.Objects;
+
 import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
 
+    @Override
+    protected void onNewIntent(@NonNull Intent intent) {
+        super.onNewIntent(intent);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            if (intent.getExtras()!=null){
+                Log.d("MainActivity Flutter", "onNewIntent() called with: intent = [" + new Gson().toJson(intent.getExtras()) + "]");
+                CleverTapAPI cleverTapAPI = CleverTapAPI.getDefaultInstance(getApplicationContext());
+                if (cleverTapAPI != null) {
+                    cleverTapAPI.pushNotificationClickedEvent(intent.getExtras());
+                }
+            }
+        }
+}
 }
